6f57d1be89acaffdd261034ade60a723b81dbfce
Sammy Samkough <sammysamkough@gmail.com>
2020-04-26 10:53:59 -0400

    Created post components

--- src/components/BoardPost.tsx
+++ src/components/BoardPost.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Row, Col } from "antd";
 
 const BoardPost = () => {
   return (
-    <Layout className="page-layout about-you-layout">
-      <div></div>
+    <Layout className="board-post">
+      <Row className="">
+        <Col span={4}>views : 132</Col>
+        <Col span={20}>maximillian</Col>
+      </Row>
+      <Row justify="space-between">
+        <Col span={2}>share</Col>
+        <Col span={2}>report</Col>
+        <Col span={2}>34 comments</Col>
+        <Col span={12}>submitted 4 hours ago</Col>
+      </Row>
     </Layout>
   );
 };
--- src/components/NewsPost.tsx
+++ src/components/NewsPost.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Row, Col } from "antd";
+
+import NewsPostImage from "./../assets/images/news-post-template.png";
 
 const NewsPost = () => {
   return (
-    <Layout className="page-layout about-you-layout">
-      <div></div>
+    <Layout className="news-post">
+      <Row className="">
+        <Col span={4}>image</Col>
+        <Col span={20}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam
+        </Col>
+      </Row>
+      <br />
+      <br />
+      <Row>
+        <Col span={12}>share</Col>
+        <Col span={12}>submitted 4 hours ago</Col>
+      </Row>
     </Layout>
   );
 };
