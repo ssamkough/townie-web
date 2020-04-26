ed1d7802490ec78fe25a34556982dd8696c86ae4
Sammy Samkough <sammysamkough@gmail.com>
2020-04-26 10:55:04 -0400

    Added faker data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,54 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
 
 import Navbar from "../components/Navbar";
 import Footie from "../components/Footie";
+import CityTemplate from "./../assets/images/home-city-template.jpg";
 
 const Home = () => {
   const { Header, Content, Footer } = Layout;
+  const { Title, Text } = Typography;
 
   return (
     <Layout className="page-layout about-you-layout">
       <Header className="board-header">
         <Navbar state={"home"} />
       </Header>
-      <Content className="about-you-content"></Content>
-      <Footer>
-        <Footie></Footie>
-      </Footer>
+      <Content className="about-you-content">
+        <Title>New York</Title>
+        <Title level={2}>New York City</Title>
+        <img
+          src={CityTemplate}
+          alt="new-york-city-template"
+          className="city-template"
+        />
+        <br />
+        <Text className="gamja-flower-fixed-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+        <Footer>
+          <Footie></Footie>
+        </Footer>
+      </Content>
     </Layout>
   );
 };
