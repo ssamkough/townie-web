5e5fa3cea52ef5a287601aab3a591e066d58e833
Sammy Samkough <sammysamkough@gmail.com>
2020-04-26 08:45:24 -0400

    Updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import GettingStarted from "./pages/GettingStarted";
+import AboutYou from "./pages/onboarding/AboutYou";
+import ConfirmLocation from "./pages/onboarding/ConfirmLocation";
+import Home from "./pages/Home";
+import News from "./pages/News";
+import Board from "./pages/Board";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <p>Townie</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <GettingStarted />
+        </Route>
+        <Route path="/about-you">
+          <AboutYou />
+        </Route>
+        <Route path="/confirm-location">
+          <ConfirmLocation />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/news">
+          <News />
+        </Route>
+        <Route path="/board">
+          <Board />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
